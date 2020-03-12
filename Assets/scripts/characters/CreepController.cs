30377aaf81ebec081faae3374431b84f5bea7210
calebe611 <calebe.c.cruz@gmail.com>
2020-03-12 08:35:02 -0300

    basic combat system

--- Assets/scripts/characters/CreepController.cs
+++ Assets/scripts/characters/CreepController.cs
@@ -8,16 +8,29 @@ using NavGame.Core;
 public class CreepController : CombatGameObject
 {
     NavMeshAgent agent;
-    GameObject finalTarget;
-    void Awake()
+    DamageableGameObject finalTarget;
+    protected override void Awake()
     {
+        base.Awake();
         agent = GetComponent<NavMeshAgent>();
         GameObject obj = GameObject.FindWithTag("Finish");
         if(obj != null)
         {
-            finalTarget = obj;
+            finalTarget = obj.GetComponent<DamageableGameObject>();
         }
 
+    }
+    protected override void Update()
+    {
+        base.Update();
+        if(finalTarget == null)
+        {
+            return;
+        }
+        if(IsInTouch(finalTarget))
+        {
+            AttackOnCooldonw(finalTarget);
+        }
     }
 
     void Start()

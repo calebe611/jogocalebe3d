8e2d895b4fe3213114a8c8b960f5af04726be3fe
calebe611 <calebe.c.cruz@gmail.com>
2020-03-17 11:52:25 -0300

    incomplete HealthUIcontroler

---
+++ Assets/scripts/NavGame/Core/HealthUIController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace NavGame.Core
+{
+    [RequireComponent(typeof(DamageableGameObject))]
+    public class HealthUIController : MonoBehaviour
+    {
+        public GameObject healthUIPrefab;
+
+        public Transform healthPosition;
+
+        GameObject healthUI;
+        Transform cam;
+
+        void Awake()
+        {
+            Canvas canvas = FindWorldCanvas();
+            if(canvas == null)
+            {
+                throw new Exception("a WorldSpace canvas was neded"); 
+            }
+            cam = Camera.main.transform;
+            healthUI = Instantiate(healthUIPrefab, canvas.transform);
+        }
+        void LateUpdate()
+        {
+
+        }
+
+        Canvas FindWorldCanvas()
+        {
+            foreach (Canvas c in FindObjectsOfType<Canvas>())
+            {
+                if(c. renderMode == RenderMode.WorlsSpace)
+                {
+                    return c;
+                }
+            }
+            return null;
+
+        }
+
+
+
+    }
+}
+

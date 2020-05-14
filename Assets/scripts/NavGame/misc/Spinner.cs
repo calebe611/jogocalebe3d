48bd4c572ec44a4a1bc45ed17e764da8ac1c5f96
calebe611 <calebe.c.cruz@gmail.com>
2020-05-14 20:10:02 -0300

    Projectile Spinner

---
+++ Assets/scripts/NavGame/misc/Spinner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace NavGame.Misc
+{
+    public class Spinner : MonoBehaviour
+    {
+        public Vector3 eulerPerSecond;
+
+        void Update()
+        {
+            transform.Rotate(eulerPerSecond * Time.deltaTime);
+        }
+    }
+}

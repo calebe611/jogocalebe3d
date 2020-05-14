5cc947e92d211d08140c7411626806a601db5088
calebe611 <calebe.c.cruz@gmail.com>
2020-05-14 11:35:50 -0300

    RedParticles prefab

---
+++ Assets/scripts/NavGame/misc/DestroyWhihDelay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Misc
+{
+    public class DestroyWhihDelay : MonoBehaviour
+    {
+        public float delay = 2f;
+        void Start()
+        {
+            Destroy(gameObject, delay);
+        }
+
+    }
+}

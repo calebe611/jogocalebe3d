6a043becc6455c374b06c4eaead8a2346b9b606f
calebe611 <calebe.c.cruz@gmail.com>
2020-05-14 21:58:05 -0300

    towercontroller

---
+++ Assets/scripts/characters/TowerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NavGame.Core;
+
+
+public class TowerController : ThrowerGameObject
+{
+    protected override void UpdateAttack()
+    {
+        if (enemiesToAttack.Count > 0)
+        {
+
+
+            FaceObjectFrame(enemiesToAttack[0].gameObject.transform);
+            AttackOnCooldonw(enemiesToAttack[0]);
+
+        }
+    }
+}
